1769e7f2db9ac6c17438e9f52ccddf69763a866f
Donard97 <donardgolaj@gmail.com>
2022-02-23 16:48:32 +0100

    Install linters and fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+/* eslint radix: ["error", "as-needed"] */
+
 const progressBars = document.querySelectorAll('span');
 
 const updateBars = () => {
-  for(i=0; i<5; i++) {
+  let i;
+
+  for (i = 0; i < 5; i++) {
     while (parseInt(progressBars[i].innerText) < 100) {
-      progressBars[i].style.width = `${parseInt(progressBars[i].innerText) +1}%`;
-      progressBars[i].innerText = `${parseInt(progressBars[i].innerText) +1}%`
+      progressBars[i].style.width = `${parseInt(progressBars[i].innerText) + 1}%`;
+      progressBars[i].innerText = `${parseInt(progressBars[i].innerText) + 1}%`;
     }
   }
 };
 
-document.querySelector('button').addEventListener ('click', updateBars)
+document.querySelector('button').addEventListener('click', updateBars);
