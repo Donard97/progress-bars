14309dd17c9c0c44e714820f93518d833012fd00
Donard97 <donardgolaj@gmail.com>
2022-02-23 17:09:17 +0100

    Fix eslint errors

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
-/* eslint radix: ["error", "as-needed"] */
-
 const progressBars = document.querySelectorAll('span');
 
 const updateBars = () => {
   let i;
 
-  for (i = 0; i < 5; i++) {
-    while (parseInt(progressBars[i].innerText) < 100) {
-      progressBars[i].style.width = `${parseInt(progressBars[i].innerText) + 1}%`;
-      progressBars[i].innerText = `${parseInt(progressBars[i].innerText) + 1}%`;
+  for (i = 0; i < 5; i += 1) {
+    while (parseInt(progressBars[i].innerText, 10) < 100) {
+      progressBars[i].style.width = `${parseInt(progressBars[i].innerText, 10) + 1}%`;
+      progressBars[i].innerText = `${parseInt(progressBars[i].innerText, 10) + 1}%`;
     }
   }
 };
+
+document.querySelector('button').addEventListener('click', updateBars);
 
 document.querySelector('button').addEventListener('click', updateBars);
